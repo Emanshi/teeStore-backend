e07c002a18435cabecd811b04df6e048d4d52c7b
stubh505 <kaustubh.sarkar505@gmail.com>
2020-05-02 22:57:58 +0530

    Added ID generation strategies and added data to db

---
+++ backend/src/main/java/com/teestore/backend/entity/generator/AddressIdGenerator.java
@@ -0,0 +1,36 @@
+package com.teestore.backend.entity.generator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AddressIdGenerator implements IdentifierGenerator {
+
+    @Override
+    public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException {
+        String prefix = "A";
+        Connection connection = sharedSessionContractImplementor.connection();
+
+        try {
+            Statement statement=connection.createStatement();
+
+            ResultSet set=statement.executeQuery("select count(address_id) as Id from Address");
+
+            if(set.next())
+            {
+                int id=set.getInt(1)+1001;
+                return prefix + id;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
