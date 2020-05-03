251f1c383e31c26eb64bad4fde4d453b17c9fbdb
stubh505 <kaustubh.sarkar505@gmail.com>
2020-05-03 12:00:28 +0530

    Added user and address entity and models

--- backend/src/main/java/com/teestore/backend/entity/UserEntity.java
+++ backend/src/main/java/com/teestore/backend/entity/UserEntity.java
@@ -1,4 +1,71 @@
 package com.teestore.backend.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Users")
 public class UserEntity {
+    @Id
+    @GenericGenerator(name = "uIdGen", strategy = "com.teestore.backend.entity.generator.UserIdGenerator")
+    @GeneratedValue(generator = "uIdGen")
+    private String userId;
+    private String userName;
+    private String emailId;
+    private String contactNumber;
+    private String password;
+    private LocalDateTime dateOfBirth;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
--- backend/src/main/java/com/teestore/backend/model/User.java
+++ backend/src/main/java/com/teestore/backend/model/User.java
@@ -1,4 +1,60 @@
 package com.teestore.backend.model;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private String userId;
+    private String userName;
+    private String emailId;
+    private String contactNumber;
+    private String password;
+    private LocalDateTime dateOfBirth;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
