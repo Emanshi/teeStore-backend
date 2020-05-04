1af0b0ef10132a11c82f2405da349773ce41ed87
stubh505 <kaustubh.sarkar505@gmail.com>
2020-05-04 19:11:52 +0530

    Added methods in user service interface

--- backend/src/main/java/com/teestore/backend/service/UserService.java
+++ backend/src/main/java/com/teestore/backend/service/UserService.java
@@ -1,4 +1,14 @@
 package com.teestore.backend.service;
 
+import com.teestore.backend.model.Address;
+import com.teestore.backend.model.User;
+
 public interface UserService {
+
+    public User addUser(User user) throws Exception;
+    public User loginUser(User user) throws Exception;
+    public User getUser(String userId) throws Exception;
+    public String editUser(String userId,User user) throws Exception;
+    public String addAddress(String userId, Address address) throws Exception;
+    public String deleteAddress(String userId,String addressId) throws Exception;
 }
