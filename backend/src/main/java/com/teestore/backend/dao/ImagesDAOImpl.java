6702990c15a3e182293e395729be69ca36572273
Emanshi <emanshisharma@gmail.com>
2020-06-03 20:39:10 +0530

    Images DAO and Service Structure Created

---
+++ backend/src/main/java/com/teestore/backend/dao/ImagesDAOImpl.java
@@ -0,0 +1,29 @@
+package com.teestore.backend.dao;
+
+import com.teestore.backend.model.Images;
+
+import java.util.List;
+
+public class ImagesDAOImpl implements ImagesDAO{
+
+
+    @Override
+    public List<Images> getImagesByReference(String reference) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<String> addImages(List<Images> imagesList) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<String> removeImages(List<String> imagesIdList) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String removeImage(String imageId) throws Exception {
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/teestore/backend/service/ImagesServiceImpl.java
@@ -0,0 +1,27 @@
+package com.teestore.backend.service;
+
+import com.teestore.backend.model.Images;
+
+import java.util.List;
+
+public class ImagesServiceImpl implements ImagesService {
+    @Override
+    public List<Images> getImagesByReference(String reference) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<String> addImages(List<Images> imagesList) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<String> removeImages(List<String> imagesIdList) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String removeImage(String imageId) throws Exception {
+        return null;
+    }
+}
