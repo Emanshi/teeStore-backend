caedf1d45cbc7d44c94e40291fee8d2afb49c74e
stubh505 <kaustubh.sarkar505@gmail.com>
2020-08-04 19:25:57 +0530

    Enabled Swagger

--- backend/src/main/java/com/teestore/backend/BackendApplication.java
+++ backend/src/main/java/com/teestore/backend/BackendApplication.java
@@ -19,6 +19,6 @@ public class BackendApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.teestore.backend")).build();
 	}
 }
