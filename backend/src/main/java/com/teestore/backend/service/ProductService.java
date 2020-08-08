08c33e90cc5b446342207e09de0223fd8e60bf7c
stubh505 <kaustubh.sarkar505@gmail.com>
2020-08-08 19:11:56 +0530

    Added ProductService Java docs

--- backend/src/main/java/com/teestore/backend/service/ProductService.java
+++ backend/src/main/java/com/teestore/backend/service/ProductService.java
@@ -14,10 +14,50 @@ public interface ProductService {
      * @throws Exception Unable to persist new product
      */
     String addNewProduct(Product product) throws Exception;
+
+    /**
+     * Service method to retrieve product by id
+     * @param productId Id of product to be retrieved
+     * @return Product
+     * @throws Exception Not Found
+     */
     Product getProductById(String productId) throws Exception;
+
+    /**
+     * Service method to get product by category
+     * @param category Product category
+     * @return List of retrieved product
+     * @throws Exception Not found
+     */
     List<Product> getProductsByCategory(Category category) throws Exception;
+
+    /**
+     * Service method to get all product
+     * @return List of retrieved product
+     * @throws Exception Not found
+     */
     List<Product> getAllProducts() throws Exception;
+
+    /**
+     * Service method to get newly arrived products
+     * @return List of retrieved product
+     * @throws Exception Not found
+     */
     List<Product> getNewArrivals() throws Exception;
+
+    /**
+     * Service method to get product by category which has a discount
+     * @param category Product category
+     * @return List of retrieved product
+     * @throws Exception Not found
+     */
     List<Product> getProductByDiscount(Category category) throws Exception;
+
+    /**
+     * Service method to get product by search term
+     * @param search Search term provided
+     * @return List of retrieved product
+     * @throws Exception Not found
+     */
     List<Product> getProductBySearch(String search) throws Exception;
 }
